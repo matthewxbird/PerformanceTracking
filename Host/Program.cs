f2a3e020dab12da18b903b578018670b2ed6f838
Matthew Bird <mbird1989@hotmail.com>
2021-06-25 11:14:20 +0100

    Add project files.

---
+++ Host/Program.cs
@@ -0,0 +1,33 @@
+﻿using PerformaceTracker;
+using System;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Started");
+
+            for (int i = 0; i < 100; i++)
+            {
+                var result = Performance.TrackAsync(SimulateWork).Result;
+
+                Console.WriteLine($"Returned Result='{result}'");
+            }
+
+            Console.WriteLine("Stopped - Press a key to exit");
+            Console.ReadKey();
+        }
+
+        public static async Task<string> SimulateWork()
+        {
+            var r = new Random();
+
+            await Task.Delay(r.Next(1, 1000));
+
+            return $"Test String: {Guid.NewGuid()}";
+        }
+    }
+}
---
+++ PerformaceTracker/Performance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PerformaceTracker
+{
+    public static class Performance
+    {
+        public static async Task<TResult> TrackAsync<TResult>(Func<Task<TResult>> method)
+        {
+            var sw = Stopwatch.StartNew();
+            var result = await method();
+            sw.Stop();
+            LogToKinesis($"TT='{sw.ElapsedMilliseconds}ms'");
+
+            return result;
+        }
+
+        public static void LogToKinesis(string log)
+        {
+            Console.WriteLine($"PL: {log}");
+        }
+    }
+}
